0e81730fdfc28cde877b791aa6133c707b87a6e8
mncedisimavunqela <166057648+mncedisimavunqela@users.noreply.github.com>
2024-06-10 08:51:44 +0200

    Update run_selenium_proot_render.sh

--- run_selenium_proot_render.sh
+++ run_selenium_proot_render.sh
@@ -2,4 +2,5 @@
 wget -O - https://raw.githubusercontent.com/mncedisimavunqela/selenium-wsl2-ubuntu/main/install-selenium.sh | bash
 sleep 2
 wget https://raw.githubusercontent.com/mncedisimavunqela/selenium-wsl2-ubuntu/main/run_selenium_proot_render.py
+pip3 install selenium
 python3 run_selenium_proot_render.py
